096d8f4f4ae1498395621789d065920e83ad6557
Day404 <69839966+Day404@users.noreply.github.com>
2021-01-20 14:54:48 +0200

    Created a method to switch between the pickup directions for the enemies

--- Gade Sup/GameEngine.cs
+++ Gade Sup/GameEngine.cs
@@ -203,14 +203,118 @@ namespace Gade_Sup
                 EnemyMove(Enemies[i].NewTile, i);
             }
         }
+        private void EnemyPickup(int Direction, int Index)
+        {
+            switch (Direction)
+            {
+                case 0:
+                    if (Enemies[Index].Vision[0].NewTile == Tile.TileType.Gold)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY + 1 == Enemies[Index].varY && GameMap.Items[i].varX == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+                                //H.Wallet.GoldDrop += Ran.Next(1, 6);
+                            }
+                        }
+                    }
+                    else if (Enemies[Index].Vision[0].NewTile == Tile.TileType.Weapon)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY + 1 == Enemies[Index].varY && GameMap.Items[i].varX == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+
+                            }
+                        }
+                    }
+                    break;
+                case 1:
+                    if (Enemies[Index].Vision[1].NewTile == Tile.TileType.Gold)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY - 1 == Enemies[Index].varY && GameMap.Items[i].varX == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+                                //H.Wallet.GoldDrop += Ran.Next(1, 6);
+                            }
+                        }
+                    }
+                    else if (Enemies[Index].Vision[1].NewTile == Tile.TileType.Weapon)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY - 1 == Enemies[Index].varY && GameMap.Items[i].varX == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    if (Enemies[Index].Vision[2].NewTile == Tile.TileType.Gold)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY  == Enemies[Index].varY && GameMap.Items[i].varX + 1 == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+                                //H.Wallet.GoldDrop += Ran.Next(1, 6);
+                            }
+                        }
+                    }
+                    else if (Enemies[Index].Vision[2].NewTile == Tile.TileType.Weapon)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY  == Enemies[Index].varY && GameMap.Items[i].varX + 1 == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    if (Enemies[Index].Vision[3].NewTile == Tile.TileType.Gold)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY  == Enemies[Index].varY && GameMap.Items[i].varX -1 == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+                                //H.Wallet.GoldDrop += Ran.Next(1, 6);
+                            }
+                        }
+                    }
+                    else if (Enemies[Index].Vision[3].NewTile == Tile.TileType.Weapon)
+                    {
+                        for (int i = 0; i < GameMap.Items.Length; i++)
+                        {
+                            if (GameMap.Items[i].varY  == Enemies[Index].varY && GameMap.Items[i].varX - 1 == Enemies[Index].varX)
+                            {
+                                Enemies[Index].PickUp(GameMap.Items[i]);
+
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
         private void EnemyMove(Tile.TileType Type, int Index)
         {
 
             switch (Type)
             {
                 case Tile.TileType.Leader:
-                    if (H.varY < Enemies[Index].varY && Enemies[Index].Vision[0].NewTile == Tile.TileType.EmptyTile)
+                    if (H.varY < Enemies[Index].varY && Enemies[Index].Vision[0].NewTile == Tile.TileType.EmptyTile || Enemies[Index].Vision[0].NewTile == Tile.TileType.Gold || Enemies[Index].Vision[0].NewTile == Tile.TileType.Weapon)
                     {
+                        
+
                         Enemies[Index].Move(Character.Movement.Up);
                         GameMap.MapDisplay[Enemies[Index].varY, Enemies[Index].varX] = Enemies[Index];
                         GameMap.MapDisplay[Enemies[Index].varY + 1, Enemies[Index].varX] = new EmptyTile(Enemies[Index].varY + 1, Enemies[Index].varX);
